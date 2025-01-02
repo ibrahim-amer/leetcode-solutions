530681f0732fc9920e62c80d4846a58f2434183b
Ibrahim Amer <ibamer92@gmail.com>
2025-01-01 21:44:52 -0500

    Time: 20 ms (37.19%), Space: 73.9 MB (32.38%) - LeetHub

--- 2559-count-vowel-strings-in-ranges/2559-count-vowel-strings-in-ranges.cpp
+++ 2559-count-vowel-strings-in-ranges/2559-count-vowel-strings-in-ranges.cpp
@@ -13,7 +13,6 @@ public:
             int status = vowels.count(words[i - 1][0]) && vowels.count(words[i - 1][wordLen - 1]) ? 1 : 0;
             prefixSum[i] = prefixSum[i - 1] + status;
         }
-        cout << endl;
         int i = -1;
         for (auto query : queries) {
             result[++i] = prefixSum[query[1] + 1] - prefixSum[query[0]];
