dffbbda22314ec3ca03d3ee8a31fc08f0a35e81c
Ibrahim Amer <ibamer92@gmail.com>
2025-01-01 21:46:39 -0500

    Time: 4 ms (81.49%), Space: 69.3 MB (74.2%) - LeetHub

--- 2559-count-vowel-strings-in-ranges/2559-count-vowel-strings-in-ranges.cpp
+++ 2559-count-vowel-strings-in-ranges/2559-count-vowel-strings-in-ranges.cpp
@@ -1,23 +1,19 @@
 class Solution {
 public:
     vector<int> vowelStrings(vector<string>& words, vector<vector<int>>& queries) {
-        int w = words.size(), q = queries.size();
-        vector<int> result(q);
-
-        unordered_set<int> vowels = {'a', 'e', 'i', 'o', 'u'};
-
-        vector<int> prefixSum(w + 1);
-
-        for (int i = 1; i <= w; i++) {
-            int wordLen = words[i - 1].length();
-            int status = vowels.count(words[i - 1][0]) && vowels.count(words[i - 1][wordLen - 1]) ? 1 : 0;
-            prefixSum[i] = prefixSum[i - 1] + status;
+        const unordered_set<char> vowels = {'a', 'e', 'i', 'o', 'u'};
+        vector<int> prefix(words.size());
+        vector<int> ans(queries.size());
+        int curr = 0, i = 0;
+        for(auto& word: words) {
+            curr += vowels.count(word[0]) && vowels.count(word[word.length()-1]) ? 1 : 0;
+            prefix[i++] = curr;
         }
-        int i = -1;
-        for (auto query : queries) {
-            result[++i] = prefixSum[query[1] + 1] - prefixSum[query[0]];
+        i = 0;
+        for(auto& query: queries) {
+            if(query[0] > 0) ans[i++] = prefix[query[1]] - prefix[query[0]-1];
+            else ans[i++] = prefix[query[1]];
         }
-
-        return result;
+        return ans;
     }
 };
